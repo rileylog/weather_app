01d3a955593749ed879e5704da7ba501d4293948
Logan Riley <rileylog@gmail.com>
2013-12-05 22:01:44 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherApp::Application.config.secret_token = '18aed301a356c4c171c710b708d7eb18ce8a5ab0c44166403bbd82110446c21ddffd1866e62debae9ce5f06fcf491e4214e03cefc319f45bb796fafae4e7389f'
